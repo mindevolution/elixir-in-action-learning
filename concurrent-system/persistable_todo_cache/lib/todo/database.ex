c62cc983695f0fcee2c8fe8666e3611991c1127a
Henry He <zhe@cn.estee.com>
2025-04-08 14:03:26 +0800

    add pool worker

--- concurrent-system/persistable_todo_cache/lib/todo/database.ex
+++ concurrent-system/persistable_todo_cache/lib/todo/database.ex
@@ -4,40 +4,44 @@ defmodule Todo.Database do
   @db_folder "./persist"
 
   def start do
-    GenServer.start(__MODULE__, nil,
-      name: __MODULE__
-    )
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
   end
 
-  def store(key, value) do
-    GenServer.cast(__MODULE__, {:store, key, value})
+  def store(key, data) do
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.get(key)
   end
 
+  # Choosing a worker makes a request to the database server process. There we
+  # keep the knowledge about our workers, and return the pid of the corresponding
+  # worker. Once this is done, the caller process will talk to the worker directly.
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
+  end
+
+  @impl GenServer
   def init(_) do
     File.mkdir_p!(@db_folder)
-    {:ok, nil}
+    {:ok, start_workers()}
   end
 
-  def handle_cast({:store, key, value}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(value))
-
-    {:noreply, state}
+  @impl GenServer
+  def handle_call({:choose_worker, key}, _, workers) do
+    worker_key = :erlang.phash2(key, 3)
+    {:reply, Map.get(workers, worker_key), workers}
   end
 
-  def handle_call({:get, key}, _from, state) do
-    case File.read(file_name(key)) do
-      {:ok, binary} -> {:reply, :erlang.binary_to_term(binary), state}
-      {:error, :enoent} -> {:reply, :not_found, state}
+  defp start_workers() do
+    for index <- 1..3, into: %{} do
+      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      {index - 1, pid}
     end
-  end
-
-  defp file_name(key) do
-    Path.join(@db_folder, "#{key}.db")
   end
 end
